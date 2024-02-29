fc8c89bc776505e7f309e8e99f2ace6a55a8b78c
Mitch Chanza <mitch.chanza@gmail.com>
2024-02-29 14:47:14 +0200

    🛠️ courses-categories.tsx -> Modified Typography and Card components

--- src/app/courses-categories.tsx
+++ src/app/courses-categories.tsx
@@ -49,27 +49,27 @@ export function CoursesCategories() {
   return (
     <section className="container mx-auto px-8 py-36">
       <div className="mb-20 grid place-items-center text-center">
-        <Typography variant="h2" color="blue-gray" className="my-3" placeholder={"categories"}>
+        <Typography  variant="h2" color="blue-gray" className="my-3" placeholder={"categories"}>
           Courses Categories
         </Typography>
-        <Typography variant="lead" className="!text-gray-500 lg:w-6/12"  placeholder={"description"}>
+        <Typography  variant="lead" className="!text-gray-500 lg:w-6/12"  placeholder={"description"}>
           A comprehensive selection of courses designed to empower you with the
           skills you need to thrive in the dynamic world of web development.
         </Typography>
       </div>
       <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
-        <Card
+        <Card 
           color="gray"
           className="relative grid h-full w-full place-items-center overflow-hidden text-center"  placeholder={undefined}        >
           <div className="absolute inset-0 h-full w-full bg-gray-900/75" />
           <CardBody className="relative w-full"  placeholder={"undefined"}>
-            <Typography color="white" className="text-xs font-bold opacity-50"  placeholder={"undefined"}>
+            <Typography  color="white" className="text-xs font-bold opacity-50"  placeholder={"undefined"}>
               HTML, CSS & Javascript
             </Typography>
             <Typography variant="h4" className="mt-9" color="white"  placeholder={"undefined"}>
               Web Development Intro
             </Typography>
-            <Typography
+            <Typography 
               color="white"
               className="mt-4 mb-14 font-normal opacity-50"  placeholder={"undefined"}            >
               Ready to start your web development journey?
